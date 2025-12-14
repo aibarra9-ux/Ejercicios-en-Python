0e6c994669a6dd768980e47a3321ea54b799a41b
aibarra9-ux <alantapon26@gmail.com>
2025-12-13 19:56:02 -0700

    Añadiendo ejercicio de iniciales

---
+++ Python_principiante/Hello_world/iniciales.py
@@ -0,0 +1,10 @@
+
+#Cree un programa que muestre sus iniciales en letras mayúsculas 
+print("       AA         IIII")   
+print("      AAAA         II")
+print("     A    A        II")
+print("    A      A       II")
+print("   A AAAAAA A      II")
+print("  A          A     II")
+print(" A            A    II")
+print("A              A  IIII")
---
+++ Python_principiante/Hello_world/patron.py
@@ -0,0 +1,11 @@
+#Cree un programa que imprima este patrón exactamente como se muestra.
+#       1
+#     2 3
+#   4 5 6
+#7 8 9 10
+
+
+print("      1")
+print("    2 3")
+print("  4 5 6")
+print("7 8 9 10")
