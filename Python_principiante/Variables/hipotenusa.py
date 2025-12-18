2547ef2033835020175b6e876d1ce15c65e8dd59
aibarra9-ux <alantapon26@gmail.com>
2025-12-17 21:30:25 -0700

    añadiendo el ejercicio de hipotenusa

---
+++ Python_principiante/Variables/hipotenusa.py
@@ -0,0 +1,7 @@
+#Cree un programa que le pida al usuario dos números, a y b
+# y luego calcule la hipotenusa c.
+
+num_a = int(input("Usuario seleccione el valor de a "))
+num_b = int(input("Usuario seleccione el valor de b "))
+hipotenusa = (num_a**2 + num_b**2) ** 0.5
+print(hipotenusa)
